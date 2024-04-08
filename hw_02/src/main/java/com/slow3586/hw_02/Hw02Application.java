da67e1396a5e662a30c3dee198e0aab0560f7132
slow3586 <6337520+slow3586@users.noreply.github.com>
2024-04-08 11:57:13 +0700

    Hw 02: Dockerfile

---
+++ hw_02/src/main/java/com/slow3586/hw_02/Hw02Application.java
@@ -0,0 +1,13 @@
+package com.slow3586.hw_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw02Application.class, args);
+    }
+
+}
